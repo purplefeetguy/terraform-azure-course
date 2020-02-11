f997a5309d2fe140f7e486bb69bf99d25a8d24d1
purplefeetguy <purplefeetguy@hotmail.com>
2020-02-10 21:27:14 -0600

    first-steps demo

---
+++ terraform-first-steps/terraform/vars.tf
@@ -0,0 +1,13 @@
+variable "location" {
+  type    = string
+  default = "northcentralus"
+}
+variable "prefix" {
+  type    = string
+  default = "demo"
+}
+
+variable "ssh-source-address" {
+  type    = string
+  default = "*"
+}
